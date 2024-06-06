124dcc93c0d132611fa0761e5e456b9e268175f2
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-06 19:08:11 +0300

    add sql_script_file and functions for use this

--- src/db/main.py
+++ src/db/main.py
@@ -1,9 +1,16 @@
 import asyncpg
 from src.config import settings
+import os
 
 
 async def create_pool():
     return await asyncpg.create_pool(settings.POSTGRES_URL)
+
+
+async def read_sql_script(file_path):
+    with open(file_path, 'r') as file:
+        sql_script = file.read()
+    return sql_script
 
 
 async def init_db():
@@ -12,3 +19,19 @@ async def init_db():
     async with pool.acquire() as connection:
         result = await connection.fetch("SELECT 'hello' AS greeting;")
         print(result)
+
+
+async def create_tables():
+    pool = await create_pool()
+    script_path = os.path.join(os.path.dirname(__file__), 'create_tables.sql')
+    sql_script = await read_sql_script(script_path)
+
+    async with pool.acquire() as connection:
+        await connection.execute(sql_script)
+
+    
+    executed_script_path = os.path.join(os.path.dirname(__file__), 'executed_tables.sql')
+    with open(executed_script_path, 'w') as file:
+        file.write(sql_script)
+
+    await pool.close()
