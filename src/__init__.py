895f3325eb422f7a30488153fff279f0f52c814a
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-06 21:58:20 +0300

    add packege websocket: exceptions, app, server (ability)

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.db.main import init_db
-from src.main import router as websocket_router
+from src.websocket.app import router as websocket_router
 
 
 @asynccontextmanager
---
+++ src/websocket/exceptions.py
@@ -0,0 +1,26 @@
+class Error(Exception):
+    code: int
+    message: str
+
+    def __init__(self, message=None):
+        if message:
+            self.message = message
+
+    @property
+    def error(self):
+        return f"Error code: {self.code}, message: {self.message}"
+
+
+class InvalidTokenException(Error):
+    code = 1
+    message = "Invalid token"
+
+
+class ExpiredTokenException(Error):
+    code = 2
+    message = "Expired token"
+
+
+class UnexpectedTokenException(Error):
+    code = 3
+    message = "Unknown token error"
