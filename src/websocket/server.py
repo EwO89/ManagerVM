5d0143fdc267c267e955d0db0b11707a0d29d65b
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-06 22:36:25 +0300

    fix dublicate func create_pool in server

--- src/websocket/server.py
+++ src/websocket/server.py
@@ -1,16 +1,13 @@
 from typing import List, Dict
 from fastapi import WebSocket, WebSocketDisconnect
 from datetime import datetime
-import asyncpg
-from src.config import settings
+from src.db.main import create_pool
+
 
 class WebSocketServer:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
         self.all_connections: List[Dict] = []
-
-    async def create_pool(self):
-        return await asyncpg.create_pool(dsn=settings.POSTGRES_URL)
 
     async def register(self, websocket: WebSocket, vm_id: str):
         await websocket.accept()
@@ -32,7 +29,8 @@ class WebSocketServer:
 
     @property
     def active_connections_list(self):
-        return [{"vm_id": conn["vm_id"], "connected_at": conn["connected_at"]} for conn in self.all_connections if conn.get("disconnected_at") is None]
+        return [{"vm_id": conn["vm_id"], "connected_at": conn["connected_at"]} for conn in self.all_connections if
+                conn.get("disconnected_at") is None]
 
     @property
     def authorized_connections_list(self):
@@ -43,7 +41,7 @@ class WebSocketServer:
         return self.all_connections
 
     async def save_connection_info(self, vm_id: str):
-        pool = await self.create_pool()
+        pool = await create_pool()
         async with pool.acquire() as connection:
             await connection.execute('''
                 INSERT INTO ws_connection_history (vm_id, connected_at)
