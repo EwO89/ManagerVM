bf63dca3d3347b61d1e5dd7b014e87eee274e676
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-07 14:55:58 +0300

    set secret_key and public key + base jwt settings

--- src/config.py
+++ src/config.py
@@ -1,6 +1,15 @@
 import os
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 from pathlib import Path
+
+# Глобальная переменная BASE_DIR
+BASE_DIR = Path(__file__).absolute().parent
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "private_key.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "public_key.pem"
+    algorithm: str = 'RS256'
 
 
 class Settings(BaseSettings):
@@ -9,13 +18,11 @@ class Settings(BaseSettings):
     WS_PORT: int
     SECRET_KEY: str
     DEBUG: bool
-    BASE_DIR: Path = Path(
-        __file__).absolute().parent
+    auth_jwt: AuthJWT = AuthJWT()
 
     class Config:
         env_file = '.env'
         extra = 'ignore'
-
 
 
 settings = Settings(_env_file=os.getenv("ENV_FILE", ".env"))
