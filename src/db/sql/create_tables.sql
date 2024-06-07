1b420687d1709ef0703dfffccc9bd7d64055e39e
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-07 23:49:59 +0300

    *test_branch update schemas sinh with db

--- src/db/sql/create_tables.sql
+++ src/db/sql/create_tables.sql
@@ -1,14 +1,26 @@
+
 CREATE TABLE IF NOT EXISTS virtual_machine (
     vm_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     ram INTEGER NOT NULL,
     cpu INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
+
+
+CREATE TABLE IF NOT EXISTS vm_disk (
+    disk_id SERIAL PRIMARY KEY,
+    vm_id INTEGER NOT NULL,
+    disk_size INTEGER NOT NULL,
+    FOREIGN KEY (vm_id) REFERENCES virtual_machine(vm_id)
+);
+
 
 CREATE TABLE IF NOT EXISTS ws_connection_history (
     id SERIAL PRIMARY KEY,
-    vm_id VARCHAR(100) NOT NULL,
+    vm_id INTEGER NOT NULL,
     connected_at TIMESTAMP NOT NULL,
-    disconnected_at TIMESTAMP
+    disconnected_at TIMESTAMP,
+    FOREIGN KEY (vm_id) REFERENCES virtual_machine(vm_id)
 );
+git
--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 from typing import Optional
 from datetime import datetime
 
+
+# Схемы для виртуальных машин
 
 class VirtualMachineCreate(BaseModel):
     name: str
@@ -19,12 +21,33 @@ class VirtualMachine(BaseModel):
     created_at: datetime
 
 
-class CreateUser(BaseModel):
-    model_config = ConfigDict(strict=True)
-    username: str
-    password: bytes
-    email: EmailStr | None = None
-    active: bool = True
+
+
+class VMDiskCreate(BaseModel):
+    vm_id: int
+    disk_size: int
+
+
+class VMDisk(BaseModel):
+    disk_id: int
+    vm_id: int
+    disk_size: int
+
+
+
+
+class WSConnectionHistoryCreate(BaseModel):
+    vm_id: int
+    connected_at: datetime
+
+
+class WSConnectionHistory(BaseModel):
+    id: int
+    vm_id: int
+    connected_at: datetime
+    disconnected_at: Optional[datetime] = None
+
+
 
 
 class TokenInfo(BaseModel):
