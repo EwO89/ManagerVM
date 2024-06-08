2eb39f0fb7fc5c0e010de9bea7328e55f6f3de44
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-08 13:35:41 +0300

    fix invalid base/dir in config

--- src/config.py
+++ src/config.py
@@ -1,9 +1,8 @@
-import os
-from pydantic import BaseSettings, BaseModel
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 from pathlib import Path
 
-
-BASE_DIR = Path(__file__).absolute().parent
+BASE_DIR = Path(__file__).absolute().parent.parent
 
 
 class AuthJWT(BaseModel):
@@ -12,12 +11,14 @@ class AuthJWT(BaseModel):
     algorithm: str = 'RS256'
     access_token_expire_minutes: int = 15
 
+
 class Settings(BaseSettings):
     POSTGRES_URL: str
     WS_HOST: str
     WS_PORT: int
     SECRET_KEY: str
     DEBUG: bool
+    BASE_DIR: Path = BASE_DIR
     auth_jwt: AuthJWT = AuthJWT()
 
     class Config:
@@ -25,4 +26,4 @@ class Settings(BaseSettings):
         extra = 'ignore'
 
 
-settings = Settings(_env_file=os.getenv("ENV_FILE", ".env"))
+settings = Settings()
