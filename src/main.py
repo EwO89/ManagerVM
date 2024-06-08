7883c6459a7d4aef77cfb2fc2131d16709f2e7d9
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-08 12:51:38 +0300

    del runserver.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+
 from fastapi import FastAPI
+
+from src.config import settings
 from src.websocket.app import router as websocket_router
+
 
 app = FastAPI(
     title="ManagerVM",
@@ -7,3 +11,9 @@ app = FastAPI(
     description="Manager VM service",
 )
 app.include_router(websocket_router)
+
+
+if __name__ == "main":
+    import uvicorn
+
+    uvicorn.run("main:app", host=settings.WS_HOST, port=settings.WS_PORT, reload=settings.DEBUG)
