5c040d9a40a294a1a12f41d546d1e54623b1a621
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-08 17:52:06 +0300

    refactor connection_his (dao) and rename class in vm_dao

--- src/db/dao/connection_history.py
+++ src/db/dao/connection_history.py
@@ -1,43 +1,42 @@
 from datetime import datetime
 from typing import List, Dict
 
-from src.config import settings
 import asyncpg
+
+from src.config import settings
+
+
+class BaseDao:
+    def __init__(self, pool):
+        self.pool = pool
 
 
 class ConnectionHistoryDao:
     def __init__(self):
-        self.pool = None
-
-    async def create_pool(self):
-        if self.pool is None:
-            self.pool = await asyncpg.create_pool(settings.POSTGRES_URL)
+        self.table_name = 'ws_connection_history'
 
     async def create(self, vm_id: int):
-        await self.create_pool()
         async with self.pool.acquire() as connection:
-            await connection.execute('''
-                INSERT INTO ws_connection_history (vm_id, connected_at)
+            await connection.execute(f'''
+                INSERT INTO {self.table_name} (vm_id, connected_at)
                 VALUES ($1, $2)
             ''', vm_id, datetime.utcnow())
 
     async def get_all_distinct(self) -> List[Dict]:
-        await self.create_pool()
         async with self.pool.acquire() as connection:
-            rows = await connection.fetch('''
+            rows = await connection.fetch(f'''
                 SELECT DISTINCT vm_id, MIN(connected_at) as first_connected_at
-                FROM ws_connection_history
+                FROM {self.table_name}
                 GROUP BY vm_id
                 ORDER BY first_connected_at
             ''')
             return [dict(row) for row in rows]
 
     async def get_all(self) -> List[Dict]:
-        await self.create_pool()
         async with self.pool.acquire() as connection:
-            rows = await connection.fetch('''
+            rows = await connection.fetch(f'''
                 SELECT vm_id, connected_at
-                FROM ws_connection_history
+                FROM {self.table_name}
                 ORDER BY connected_at
             ''')
             return [dict(row) for row in rows]
--- src/websocket/server.py
+++ src/websocket/server.py
@@ -4,13 +4,13 @@ from src.auth.utils import encode_jwt
 from src.config import settings
 from src.schemas import VirtualMachine, WSConnectionHistoryCreate, VirtualMachineCreate, VMDiskCreate
 from datetime import datetime
-from src.db.dao.virtual_machine_DAO import VirtualMachineDAO
+from src.db.dao.virtual_machine import VirtualMachineDAO
 from src.db.main import create_pool
 
 
 class WebsocketServer:
     def __init__(self, pool):
-        self.virtual_machine_dao = VirtualMachineDAO(pool)
+        self.virtual_machine = VirtualMachineDAO(pool)
         self.connection_history = []
 
     async def connect_to_client(self, vm: VirtualMachine):
@@ -54,15 +54,15 @@ class WebsocketServer:
             self.connection_history[-1].disconnected_at = datetime.utcnow()
 
     async def list_connected_clients(self):
-        vms = await self.virtual_machine_dao.list_vms()
+        vms = await self.virtual_machine.list_vms()
         return [vm.dict() for vm in vms if vm.is_connected]
 
     async def list_authorized_clients(self):
-        vms = await self.virtual_machine_dao.list_vms()
+        vms = await self.virtual_machine.list_vms()
         return [vm.dict() for vm in vms if vm.is_authenticated]
 
     async def list_all_disks(self):
-        vms = await self.virtual_machine_dao.list_vms()
+        vms = await self.virtual_machine.list_vms()
         disks = []
         for vm in vms:
             for disk in vm.hard_disks:
@@ -75,7 +75,7 @@ class WebsocketServer:
         return [history.dict() for history in self.connection_history]
 
     async def disconnect_client(self, vm_id: int):
-        vm = await self.virtual_machine_dao.get_vm(vm_id)
+        vm = await self.virtual_machine.get_vm(vm_id)
         if vm and vm.is_authenticated:
             await self.close_connection(vm)
 
@@ -90,7 +90,7 @@ class WebsocketServer:
             self.connection_history[-1].disconnected_at = datetime.utcnow()
 
     async def update_vm(self, vm_id: int, ram: int = None, cpu: int = None, description: str = None):
-        vm = await self.virtual_machine_dao.get_vm(vm_id)
+        vm = await self.virtual_machine.get_vm(vm_id)
         if vm:
             updated_vm = VirtualMachineCreate(
                 name=vm.name,
@@ -100,7 +100,7 @@ class WebsocketServer:
                 uri=vm.uri,
                 hard_disks=[VMDiskCreate(disk_id=disk.disk_id, disk_size=disk.disk_size) for disk in vm.hard_disks]
             )
-            await self.virtual_machine_dao.update_vm(vm_id, updated_vm)
+            await self.virtual_machine.update_vm(vm_id, updated_vm)
             print(f"Updated VM {vm_id}: {updated_vm.dict()}")
 
     async def run(self):
@@ -127,8 +127,8 @@ class WebsocketServer:
             ]
         )
 
-        vm1_id = await self.virtual_machine_dao.create_vm(vm1)
-        vm2_id = await self.virtual_machine_dao.create_vm(vm2)
+        vm1_id = await self.virtual_machine.create_vm(vm1)
+        vm2_id = await self.virtual_machine.create_vm(vm2)
 
-        vms = [await self.virtual_machine_dao.get_vm(vm1_id), await self.virtual_machine_dao.get_vm(vm2_id)]
+        vms = [await self.virtual_machine.get_vm(vm1_id), await self.virtual_machine.get_vm(vm2_id)]
         await asyncio.gather(*[self.connect_to_client(vm) for vm in vms if vm is not None])
